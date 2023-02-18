1ee73cbd8003078a2a6dab9368f90b9ab6b8a8d4
volodymyr-havryliuk165 <vova2341591@gmail.com>
2023-02-18 03:20:32 +0200

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,3 +1,4 @@
+local M = {}
 local wezterm = require 'wezterm'
 local terafox_colors = {
     base = "#0B1314",
@@ -11,65 +12,66 @@ local terafox_colors = {
     blue = "#73A3B7",
     light_blue = "#AFD4DE",
 }
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    M.default_prog = { 'wsl' }
+end
 
-return {
-    color_scheme_dirs = { wezterm.config_dir .. '/custom_themes' },
-    color_scheme = "terafox",
-    background = {
-        {
-            source = {
-                File = {
-                    path = wezterm.config_dir .. '/wallpapers/better_foresty_anim.png',
-                    speed = 0.5
-                }
-            },
-            width = '100%',
-            repeat_x = 'NoRepeat',
-            hsb = {
-                brightness = 0.05,
-            },
-        },
-    },
-    window_frame = {
-        inactive_titlebar_bg = terafox_colors.dark_green,
-        active_titlebar_bg = terafox_colors.base,
-    },
-    window_decorations = "RESIZE",
-    colors = {
-
-        tab_bar = {
-
-            inactive_tab_edge = terafox_colors.green,
-
-            active_tab = {
-                bg_color = terafox_colors.dark_green,
-                fg_color = terafox_colors.dark_pink,
-            },
-
-            inactive_tab = {
-                bg_color = terafox_colors.dark_green,
-                fg_color = terafox_colors.white,
-            },
-
-            inactive_tab_hover = {
-                bg_color = terafox_colors.base,
-                fg_color = terafox_colors.red,
-            },
-
-            new_tab = {
-                bg_color = terafox_colors.dark_green,
-                fg_color = terafox_colors.white,
-            },
-
-            new_tab_hover = {
-                bg_color = terafox_colors.base,
-                fg_color = terafox_colors.red,
+M.color_scheme_dirs = { wezterm.config_dir .. '/custom_themes' }
+M.color_scheme = "terafox"
+M.background = {
+    {
+        source = {
+            File = {
+                path = wezterm.config_dir .. '/wallpapers/better_foresty_anim.png',
+                speed = 0.5
             }
-
         },
-
-    },
-    inactive_pane_hsb = {
-        brightness = 0.4,
+        width = '100%',
+        repeat_x = 'NoRepeat',
+        hsb = {
+            brightness = 0.05,
+        },
     },
 }
+M.window_frame = {
+    inactive_titlebar_bg = terafox_colors.dark_green,
+    active_titlebar_bg = terafox_colors.base,
+}
+M.window_decorations = "RESIZE"
+M.colors = {
+    tab_bar = {
+
+        inactive_tab_edge = terafox_colors.green,
+
+        active_tab = {
+            bg_color = terafox_colors.dark_green,
+            fg_color = terafox_colors.pink,
+        },
+
+        inactive_tab = {
+            bg_color = terafox_colors.dark_green,
+            fg_color = terafox_colors.white,
+        },
+
+        inactive_tab_hover = {
+            bg_color = terafox_colors.base,
+            fg_color = terafox_colors.dark_pink,
+        },
+
+        new_tab = {
+            bg_color = terafox_colors.dark_green,
+            fg_color = terafox_colors.white,
+        },
+
+        new_tab_hover = {
+            bg_color = terafox_colors.base,
+            fg_color = terafox_colors.dark_pink,
+        }
+
+    },
+}
+M.inactive_pane_hsb = {
+    brightness = 0.4,
+}
+
+return M
