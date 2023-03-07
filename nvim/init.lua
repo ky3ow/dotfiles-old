82ed07b877bb9819962c75c0a9593b56a8eec40b
volodymyr-havryliuk165 <vova2341591@gmail.com>
2023-03-07 19:11:33 +0200

    Change init.lua require order to fix things up

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
-require "ky3ow.lazy"
-require "ky3ow.keymaps"
 require "ky3ow.options"
+require "ky3ow.keymaps"
+require "ky3ow.lazy"
 require "ky3ow.lsp"
 require "ky3ow.telescope"
 require "ky3ow.treesitter"
--- nvim/lua/ky3ow/options.lua
+++ nvim/lua/ky3ow/options.lua
@@ -1,50 +1,51 @@
 local options = {
-    backup = false,
-    cmdheight = 2,
-    conceallevel = 0,
-    completeopt = { "menuone", "noselect" },
-    fileencoding = "utf-8",
-    gdefault = true,
-    hlsearch = false,
-    ignorecase = true,
-    mouse = "a",
-    pumheight = 10,
-    showmode = true,
-    showtabline = 2,
-    smartcase = true,
-    smartindent = true,
-    splitbelow = true,
-    splitright = true,
-    swapfile = false,
-    termguicolors = true,
-    timeoutlen = 1000,
-    undofile = true,
-    updatetime = 300,
-    writebackup = false,
-    wrap = false,
-    expandtab = true,
-    shiftwidth = 4,
-    tabstop = 4,
-    number = true,
-    cursorline = true,
-    relativenumber = true,
-    numberwidth = 4,
-    signcolumn = "yes",
-    scrolloff = 8,
-    sidescrolloff = 8,
-    list = true
+	backup = false,
+	cmdheight = 2,
+	conceallevel = 0,
+	completeopt = { "menuone", "noselect" },
+	fileencoding = "utf-8",
+	gdefault = true,
+	hlsearch = false,
+	ignorecase = true,
+	mouse = "a",
+	pumheight = 10,
+	showmode = true,
+	showtabline = 2,
+	smartcase = true,
+	smartindent = true,
+	splitbelow = true,
+	splitright = true,
+	swapfile = false,
+	termguicolors = true,
+	timeoutlen = 1000,
+	undofile = true,
+	updatetime = 300,
+	writebackup = false,
+	wrap = false,
+	expandtab = true,
+	shiftwidth = 4,
+	tabstop = 4,
+	number = true,
+	cursorline = true,
+	relativenumber = true,
+	numberwidth = 4,
+	signcolumn = "yes",
+	scrolloff = 8,
+	sidescrolloff = 8,
+	list = true
 }
 for k, v in pairs(options) do
-    vim.opt[k] = v
+	vim.opt[k] = v
 end
+
 
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = highlight_group,
-    pattern = '*',
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = '*',
 })
 
 vim.opt.shortmess:append "c"
